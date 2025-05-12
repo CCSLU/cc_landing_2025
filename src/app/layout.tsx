ccdcf0fd9c70152b407df1d92c0a3d8a943a0860
CCSLU <CASSANICASITA@GMAIL.COM>
2025-05-12 15:45:06 +0200

    Next Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,35 @@
-import "@/styles/globals.css";
-import { cn } from "@/lib";
-import { generateMetadata } from "@/utils";
-import { base, heading } from "@/constants";
-import { Toaster } from "@/components/ui/sonner";
-import { subheading } from "@/constants/fonts";
+import '@/styles/globals.css';
+import { cn } from '@/lib';
+import { generateMetadata } from '@/utils';
+import { base, heading } from '@/constants';
+import { Toaster } from '@/components/ui/sonner';
+import { subheading } from '@/constants/fonts';
 
 export const metadata = generateMetadata();
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-background text-foreground antialiased font-heading overflow-x-hidden !scrollbar-hide",
-                    base.variable,
-                    heading.variable,
-                    subheading.variable,
-                )}
-            >
-                    <Toaster richColors theme="dark" position="top-right" />
-                    {children}
-            </body>
-        </html>
-    );
-};
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning>
+			<body
+				className={cn(
+					'min-h-screen bg-background text-foreground antialiased font-heading overflow-x-hidden !scrollbar-hide',
+					base.variable,
+					heading.variable,
+					subheading.variable
+				)}>
+				<Toaster
+					richColors
+					theme='dark'
+					position='top-right'
+				/>
+				{children}
+			</body>
+		</html>
+	);
+}
